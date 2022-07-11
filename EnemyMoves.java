7fdb2acc2dbd29845040e9881f5b4548d17b18a9
wshield05 <whiteshield.tg@protonmail.com>
2022-07-11 11:28:36 +0200

    c

--- EnemyMoves.java
+++ EnemyMoves.java
@@ -1,4 +1,12 @@
 public class EnemyMoves {
 
-    //so maybe you can do s 2p mode?
+    //so maybe you can do a 2p mode? human vs human?
+    
+    //try to place the spot on the center, then on the sides, then in the rest of the space, and then check
+    //if anyone has won the game yet.
+    
+    public void placeAtCenter(Player p1, Player p2){
+        
+        if
+    }
 }
