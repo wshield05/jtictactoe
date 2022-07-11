a62f2b19dc1d46aef26bb24c95f2fa6c458d882a
wshield05 <whiteshield.tg@protonmail.com>
2022-07-11 11:51:13 +0200

    Update Player.java
    removed pointless stuff

--- Player.java
+++ Player.java
@@ -2,10 +2,11 @@ public class Player {
 
     private String name;
     private boolean isCurrentTurn;
-    private boolean currentStatus; //is it true or false? see later comment
     private char symbol;
     /*in the playfield, each player's move is represented 
     either by a false or a true in the array.
+    if a player occupies a spot, the position in the array will be true.
+    symbol is O -> 
     */
     private boolean[] playfield = {false, false, false, false, false, false, false, false, false};
     //if the player hasn't occupied that space, it's false, else it's true.
@@ -25,9 +26,6 @@ public class Player {
     public Player(String name) {
         this.name = name;
     }
-	
-    //public void startTurn ()
-            //the player chooses which symbol to use; true uses X, false uses O.
 }
 
 
