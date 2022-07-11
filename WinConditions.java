197ebc7c600a4c2d5240cb1f96eafb5ac883088b
wshield05 <whiteshield.tg@protonmail.com>
2022-07-11 11:46:04 +0200

    Update WinConditions.java
    set win conditions

--- WinConditions.java
+++ WinConditions.java
@@ -1,5 +1,23 @@
 public class WinConditions {
 	
+    public boolean hasWon(Player p2){
+        
+        if (this.playfield[7] && this.playfield[4] && this.playfield[1])
+            return true;
+        else if (this.playfield[3] && this.playfield[4] && this.playfield[5])
+            return true;
+        else if (this.playfield[7] && this.playfield[4] && this.playfield[1])
+            return true;
+        else if (this.playfield[6] && this.playfield[4] && this.playfield[2])
+            return true;
+        else if (this.playfield[0] && this.playfield[4] && this.playfield[8])
+            return true;
+        else if (this.playfield[6] && this.playfield[3] && this.playfield[0])
+            return true;
+        else if (this.playfield[8] && this.playfield[5] && this.playfield[2])
+            return true;
+    }
     
+    //refer to the scheme in Player.java to understand.
 }
 
