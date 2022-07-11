9d7da4e39209f87b1d398c918d62d9e9cde7b6b8
wshield05 <whiteshield.tg@protonmail.com>
2022-07-11 11:18:54 +0200

    added comments

--- Player.java
+++ Player.java
@@ -7,7 +7,20 @@ public class Player {
     /*in the playfield, each player's move is represented 
     either by a false or a true in the array.
     */
-
+    private boolean[] playfield = {false, false, false, false, false, false, false, false, false};
+    //if the player hasn't occupied that space, it's false, else it's true.
+    
+    /*structure of the array
+    
+        | 6 | 7 | 8 |
+        -------------
+        | 3 | 4 | 5 |
+        -------------
+        | 0 | 1 | 2 |
+        -------------
+    
+    */
+    
     public Player(String name) {
         this.name = name;
     }
